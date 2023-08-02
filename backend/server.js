57eea6cde81cb2f6b64122c7c8a012e986f5745f
Rishabh Jain <90708164+RishabhJain-11@users.noreply.github.com>
2023-08-02 20:31:02 +0530

    ...

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const cors = require('cors'); // Import cors middleware
-const port = process.env.PORT;
+const cors = require('cors');
+const port = process.env.PORT || 3000; // Default to port 3000 if PORT environment variable is not set in .env
 
 const projectRoutes = require('./routes/projects');
 const certificationRoutes = require('./routes/certifications');
@@ -12,7 +12,7 @@ const linkRoutes = require('./routes/links');
 
 app.use(express.json());
 
-app.use(cors()); // Apply cors middleware
+app.use(cors());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
@@ -24,7 +24,7 @@ app.use('/api/certifications', certificationRoutes);
 app.use('/api/articles', articleRoutes);
 app.use('/api/links', linkRoutes);
 
-mongoose.connect("mongodb://0.0.0.0:27017/portfolio")
+mongoose.connect("mongodb://localhost:27017/portfolio")
     .then(() => {
         app.listen(port, () => {
             console.log(`Connected to port ${port}`)
